f82905918ec34935531eec7fa793b284dd6a0868
m-a-t-h-a-i <mathaimarvin574@gmail.com>
2023-01-22 22:16:08 +0300

    listing army bot

---
+++ src/components/BotListArmy.js
@@ -0,0 +1,28 @@
+import React from "react";
+import CardBot from "./CardBot";
+
+function BotListArmy ({bots, removeBot, deleteBot}){
+
+    const botArmy =  bots.map((bot)=>{
+        return (
+            <CardBot
+            key={bot.id}
+            bot={bot}
+            clickEvent={removeBot}
+            deleteBot={deleteBot}
+            />
+        )
+    })
+
+    return(
+        <div className= 'd-block p-2 bg-dark text-white army'>
+            <div className="container mt-5">
+                <div className="row army-row">
+                    {botArmy}
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default BotListArmy
